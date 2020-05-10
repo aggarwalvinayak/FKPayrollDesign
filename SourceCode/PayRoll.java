d6045e2f70e712f79f5f6398f7342207b33eb364
Vinayak Aggarwal <f20170008@goa.bits-pilani.ac.in>
2020-05-10 22:15:41 +0530

    ADD Payroll Class:

---
+++ SourceCode/PayRoll.java
@@ -0,0 +1,63 @@
+import java.time.LocalDate;  
+import java.util.*;
+
+public class PayRoll
+{
+	static LocalDate lastDate;
+	static LocalDate currentRun;
+	static void payTimeCards()
+	{
+		TimeCard card=Database.getTimeCards(currentRun);
+		while(card!=null)
+		{
+			Employee e = Database.getEmployee(card.empId);
+			e.postCard(card.hours);
+			card=Database.getTimeCards(currentRun);
+		}
+	}
+	static void paySalaryReceipts()
+	{
+		SalesCard card=Database.getSalesCards(currentRun);
+		while(card!=null)
+		{
+			Employee e = Database.getEmployee(card.empId);
+			e.postCard(card.saleAmount);
+			card=Database.getSalesCards(currentRun);
+		}
+	}
+	static void unionDeduction()
+	{
+		UnionCharges card=Database.getUnionCharges(currentRun);
+		while(card!=null)
+		{
+			Employee e = Database.getEmployee(card.empId);
+			e.addDeductions(card.amount);
+			card=Database.getUnionCharges(currentRun);
+		}
+	}
+	static void pay()
+	{
+		HashMap<Integer,Employee> employees = Database.getEmployeeDatabase();
+		for (Map.Entry<Integer, Employee> employee : employees.entrySet()) 
+		{
+			if((employee.getValue()).isPayDay(currentRun))
+			{
+				System.out.println("Employee " + employee.getKey() + " paid Rs." +
+					employee.getValue().getPay() + " on " + currentRun);
+			}
+   		}
+	}
+	static void run(LocalDate date)
+	{
+		while(lastDate.isBefore(date))
+		{
+			currentRun=lastDate.plusDays(1);
+			payTimeCards();
+			paySalaryReceipts();
+			unionDeduction();
+			pay();
+			lastDate=currentRun;
+		}
+
+	}
+}
